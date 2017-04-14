6551cdb022d9c10f572914b0ac9e984a66e8d259
Eric Ober <Eric@erics-mbp.nvstu.edu>
2017-04-14 13:23:03 -0400

    stuff

--- src/main/java/com/example/EmailController.java
+++ src/main/java/com/example/EmailController.java
@@ -3,6 +3,9 @@ package com.example;
 import com.example.entities.User;
 import com.example.models.SignupRequest;
 import com.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +18,7 @@ import java.util.UUID;
 @RestController
 public class EmailController {
 
-    //private static final Logger logger = LoggerFactory.getLogger(EmailController.class);
+   //private static final Logger logger = LoggerFactory.getLogger(EmailController.class);
 
     @Inject
     public EmailController(UserRepository userRepository, Notifications notifications) {
@@ -33,8 +36,9 @@ public class EmailController {
 
         final UUID userKey = UUID.nameUUIDFromBytes(request.getEmailAddress().getBytes());
 
-        if (userRepository.exists(userKey)) {
+        if(userRepository.exists(userKey)){
             return "email already in use";
+
         }
 
         //creating a user
@@ -45,50 +49,50 @@ public class EmailController {
         user.setUserKey(userKey);
         userRepository.save(user);
 
+        //???????? do i need this simple mail message bloc if I am using my notifications below?
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(request.getEmailAddress());
         mailMessage.setFrom(request.getEmailAddress());
         mailMessage.setSubject(request.getEmailAddress());
         mailMessage.setText("Thank you for registering for Pester Your Rep. Please click on the link below to validate your account");
 
+       notifications.sendNotification(user);
 
-//        MailSender mailSender = new MailSender() {
-//
-//            public void send(mailMessage) throws MailException {
-//
-//            }
-//
-//            @Override
-//            public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
-//
-//            }
-//        }
+        MailSender mailSender = new MailSender() {
+
+            public void send(mailMessage) throws MailException {
+
+            }
+
+            @Override
+            public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
+
+            }
+        }
         //send the url for the new user to click on to validate their account
 
         //after the user is saved into our repository we send them an email to verify that their email exists.
 
-        //do i have to implement this send method twice, again with then first send method, because it goes red when i take it out
+                //do i have to implement this send method twice, again with then first send method, because it goes red when i take it out
 
         //have the server send our user an email
 //        return "Thank you for registering with Pester Your Rep.  An e-mail has been sent to your e-mail address for verification";
 
-//    }
-//
-//
-//    @RequestMapping(path = "/registration", method = RequestMethod.GET)
-//    public Iterable<User> users() {
-//
-//       return userRepository.findAll();
-//
-//
-//    }
-//
-//
-//
-//    //create a simpleMailMessage object
-//
-        return "this";
     }
+
+
+    @RequestMapping(path = "/registration", method = RequestMethod.GET)
+    public Iterable<User> users() {
+
+       return userRepository.findAll();
+
+
+    }
+
+
+
+    //create a simpleMailMessage object
+
 }
 
 
--- src/main/java/com/example/entities/VerificationToken.java
+++
@@ -1,9 +0,0 @@
-//package com.example.entities;
-//
-///**
-// * Created by Eric on 4/13/17.
-// */
-//public class VerificationToken {
-//
-//
-//}
